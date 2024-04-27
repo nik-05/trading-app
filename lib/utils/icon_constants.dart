cc8b44e8ce70eefa4aae6ea228d5c60de5b08d86
Nikhil Jindal <nik.05.jindal@gmail.com>
2024-04-27 22:14:18 +0530

    Added bottom bar icons, UI of welcome card complete

--- lib/utils/icon_constants.dart
+++ lib/utils/icon_constants.dart
@@ -2,5 +2,9 @@ class IconConstants {
   IconConstants._();
 
   static const String _iconPath = 'assets/icons/';
-
+  static const String home = '$_iconPath/home.svg';
+  static const String chart = '$_iconPath/chart.svg';
+  static const String order = '$_iconPath/order.svg';
+  static const String market = '$_iconPath/market.svg';
+  static const String profile = '$_iconPath/profile.svg';
 }
