ef7fc96a3809d5275638079049a3cc2dc0884507
Mayank Parida <mayank.parida@unicoconnect.com>
2024-04-29 17:09:01 +0530

    Completed the UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:trading_app/screens/home_screen.dart';
 import 'package:trading_app/utils/global_constants.dart';
+
+import 'modules/home/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
