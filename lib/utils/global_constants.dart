0cfb3afa1c9fc75ba86607ce25915d88a39aa0f5
Nikhil <mayank.parida@unicoconnect.com>
2024-04-29 17:11:14 +0530

    cc8b44e

--- lib/utils/global_constants.dart
+++ lib/utils/global_constants.dart
@@ -5,9 +5,14 @@ class GlobalConstants {
   static const Color textPrimary = Color(0xFF212121);
   static const Color onPrimary = Color(0xFFFFFFFF);
   static const Color primaryColor = Color(0xFF0063F5);
+  static const Color scaffoldColor = Color(0xFFF8F9FA);
+  static const Color dividerColor = Color(0xFFD9D9D9);
+  static const Color positiveColor = Color(0xFF21BF73);
+  static const Color negativeColor = Color(0xFFD90429);
+  static const Color hintColor = Color(0xFF7F7F7F);
 
   static ThemeData appTheme = ThemeData(
-    colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
+    colorScheme: ColorScheme.fromSeed(seedColor: primaryColor, ),
     useMaterial3: true,
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
